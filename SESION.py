b3ef01f32ec48ecbe21508d94638cc11b75589d0
XJMxR <128934345+XJMxR@users.noreply.github.com>
2023-10-05 19:36:05 -0500

    Update SESION.py
    Klk manito

--- SESION.py
+++ SESION.py
@@ -1,2 +1,14 @@
-
-
+class Seccion_login:
+  
+  def __init__(self) -> None:
+    pass
+  def L_facebook (self,entrada,user_list)
+  self.e_user=user_list["usuario"]
+  self.e_password=user_list["password"]
+  self.user_entrada=entrada
+  self.pass_entrada=entrada
+  if self.user_entrada == self.e_user
+    if self.pass_entrada == self.e_password
+      return "Este usuario y contraceña es correcto"
+  else
+    return "Usuario y/o contraceña incorrecto"
