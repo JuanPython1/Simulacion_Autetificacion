c59feb8360117d8378db160a79d9d50bd14ccee8
JuanR <112212338+JuanPython1@users.noreply.github.com>
2023-10-05 20:07:24 -0500

    Update SESION.py

--- SESION.py
+++ SESION.py
@@ -1,14 +1,20 @@
+
 class Seccion_login:
   
-  def __init__(self) -> None:
-    pass
-  def L_facebook (self,entrada,user_list)
-  self.e_user=user_list["usuario"]
-  self.e_password=user_list["password"]
-  self.user_entrada=entrada
-  self.pass_entrada=entrada
-  if self.user_entrada == self.e_user
-    if self.pass_entrada == self.e_password
-      return "Este usuario y contraceña es correcto"
-  else
-    return "Usuario y/o contraceña incorrecto"
+  def __init__(self):
+    self.user_list= ["juan","juan123"]
+
+  def L_facebook (self,usuario,password):
+    self.e_user=self.user_list[0]
+    self.e_password=self.user_list[1]
+    self.user_entrada=usuario
+    self.pass_entrada=password
+    if self.user_entrada == self.e_user:
+        if self.pass_entrada == self.e_password:
+            return "Este usuario y contraceña es correcto"
+    else:
+        return "Usuario y/o contraceña incorrecto"
+
+
+
+
