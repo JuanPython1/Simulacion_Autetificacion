bc684384e51274f8a08ae32cbcd4175d60f42265
JuanR <112212338+JuanPython1@users.noreply.github.com>
2023-10-07 02:18:30 -0500

    Update SESION.py

--- SESION.py
+++ SESION.py
@@ -1,20 +1,30 @@
-
 class Seccion_login:
-  
-  def __init__(self):
-    self.user_list= ["juan","juan123"]
 
-  def L_facebook (self,usuario,password):
-    self.e_user=self.user_list[0]
-    self.e_password=self.user_list[1]
-    self.user_entrada=usuario
-    self.pass_entrada=password
-    if self.user_entrada == self.e_user:
-        if self.pass_entrada == self.e_password:
-            return "Este usuario y contraceña es correcto"
-    else:
-        return "Usuario y/o contraceña incorrecto"
+    def __init__(self):
+        self.user_list = {"juan": "juan123", "Pamela": "nashe123"}
+
+    def buscarUsuario(self, nombre_usuario):
+        if nombre_usuario in self.user_list:
+            return nombre_usuario
+        return None
+
+    def buscarContraseña(self, contraseña):
+        if contraseña in self.user_list.values():
+            return contraseña
+        return None
+
+    def L_facebook(self, usuario, contraseña):
+        self.user_entrada = usuario
+        self.pass_entrada = contraseña
+
+        self.e_user = self.buscarUsuario(self.user_entrada)
+        self.e_pass = self.buscarContraseña(self.pass_entrada)
+
+        if self.user_entrada == self.e_user and self.pass_entrada == self.e_pass:
+            return "Este usuario y contraseña es correcto"
+        else:
+            return "Usuario y/o contraseña incorrecto"
 
 
 
-
+        
